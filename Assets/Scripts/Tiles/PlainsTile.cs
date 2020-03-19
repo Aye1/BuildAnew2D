9031d9652c6dc7c603e6e2da2a150711e872bb29
Adrien Janvier <janvier.adrien@gmail.com>
2020-03-19 15:43:50 +0100

    Remove old Plains debug value

--- Assets/Scripts/Tiles/PlainsTile.cs
+++ Assets/Scripts/Tiles/PlainsTile.cs
@@ -2,34 +2,9 @@
 
 public class PlainsTile : TerrainTile
 {
-    private int debugLevel = 1;
-
     public override TerrainType GetTerrainType()
     {
         return TerrainType.Plains;
     }
 
-    public override void OnTurnStarts(BaseTileData[] neighbours)
-    {
-        int upValue = 0;
-        foreach(BaseTileData tile in neighbours)
-        {
-            if (tile.terrainTile is WaterTile)
-            {
-                upValue++;
-            }
-        }
-        debugLevel += upValue;
-    }
-
-    public override string GetDebugText()
-    { 
-        return debugLevel.ToString();
-    }
-
-    public override void DebugOnClick()
-    {
-        debugLevel++;
-        base.OnSpecificTileModified();
-    }
 }
