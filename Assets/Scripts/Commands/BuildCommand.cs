cd84af2559c2d07c0d72894ed4665dbba46a9b16
Adrien Janvier <janvier.adrien@gmail.com>
2020-03-22 17:21:38 +0100

    Minor cleaning

--- Assets/Scripts/Commands/BuildCommand.cs
+++ Assets/Scripts/Commands/BuildCommand.cs
@@ -1,23 +1,24 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class BuildCommand : Command
 {
     public Vector3Int position;
     public StructureType type;
-    public Cost cost;
+    public IEnumerable<Cost> cost;
 
     public BuildCommand()
     {
         position = Vector3Int.zero;
         type = StructureType.None;
-        cost = Cost.zero;
+        cost = Cost.zeros;
     }
 
     public BuildCommand(StructureType type, Vector3Int position)
     {
         this.position = position;
         this.type = type;
-        cost = GetCostForStructureType(type);
+        cost = TilesDataManager.Instance.CostForStructure(type);
     }
 
     public override void Execute()
@@ -32,11 +33,6 @@ public class BuildCommand : Command
     public override void Undo()
     {
         TilesDataManager.Instance.RemoveStructureAtPos(position);
-    }
-
-    private Cost GetCostForStructureType(StructureType structure)
-    {
-        return structure == StructureType.None ? Cost.zero : Cost.debugCost;
     }
 }
 
