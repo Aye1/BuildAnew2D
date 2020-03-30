9a796b9f78402fede782548b7f19ec1b3c5fe74a
Alice <jade0@hotmail.fr>
2020-03-30 16:03:32 +0200

    Add energy activation cost into tooltip

--- Assets/TooltipBuildingInfo.cs
+++ Assets/TooltipBuildingInfo.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using UnityEngine.UI;
 using System.Collections.Generic;
+using System.Collections;
 
 public class TooltipBuildingInfo : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class TooltipBuildingInfo : MonoBehaviour
     [SerializeField] private GameObject childObject;
     [SerializeField] private LayoutGroup panel;
     [SerializeField] private ResourceInfo templateResourceInfo;
+    [SerializeField] private ResourceInfo energyResourceInfo;
 #pragma warning restore 0649
     #endregion
     private StructureType previousDisplayedType = StructureType.None;
@@ -21,20 +23,47 @@ public class TooltipBuildingInfo : MonoBehaviour
     void Update()
     {
         StructureType currentType = UIManager.Instance.HoveredStructure;
-        childObject.SetActive(currentType != StructureType.None);
+
         if(currentType != StructureType.None)
         {
+            childObject.SetActive(true);
             Vector3 pos = MouseManager.Instance.GetMouseScreenPos();
             childObject.transform.position = pos;
-            if(currentType != previousDisplayedType)
+            if (currentType != previousDisplayedType)
             {
-                previousDisplayedType = currentType;
-                 StructureBinding binding = TilesDataManager.Instance.GetStructureBindingFromType(currentType);
-                _buildingNameText.text = binding.data.structureName;
-                List<Cost> creationCost = binding.data.GetCreationCost();
-                CreateResourcesList(creationCost);
+                DisplayStructureInfo(currentType);
             }
         }
+        else
+        {
+           // if(currentType != previousDisplayedType)
+            {
+                childObject.SetActive(false);
+                //StartCoroutine("Fade");
+            }
+        }
+        
+        previousDisplayedType = currentType;
+    }
+    IEnumerator Fade()
+    {
+        for (float ft = 1f; ft >= 0; ft -= 0.25f)
+        {
+            Material material = childObject.gameObject.GetComponent<Renderer>().material;
+            Color c = material.color;
+            c.a =ft;
+            material.color = c;
+            yield return new WaitForSeconds(.1f);
+        }
+    }
+    private void DisplayStructureInfo(StructureType structureType)
+    {
+        StructureBinding binding = TilesDataManager.Instance.GetStructureBindingFromType(structureType);
+        _buildingNameText.text = binding.data.structureName;
+        List<Cost> creationCost = binding.data.GetCreationCost();
+        CreateResourcesList(creationCost);
+        energyResourceInfo.SetAmount(binding.data.consumedEnergyAmount);
+        energyResourceInfo.gameObject.SetActive(binding.data.ConsumesEnergy);
     }
 
     public void CreateResourcesList(List<Cost> resources)
