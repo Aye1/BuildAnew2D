2e4cbcc3b3b6e1d47a1da2551c3ee86f8d5ef317
Alice <jade0@hotmail.fr>
2020-04-05 22:37:58 +0200

    Merge fix

--- Assets/Scripts/Manager/RelayManager.cs
+++ Assets/Scripts/Manager/RelayManager.cs
@@ -8,6 +8,7 @@ public class RelayManager : MonoBehaviour
     private List<BaseTileData> _relayBastTileData;
     private IEnumerable<BaseTileData> _constructiblesTiles;
     public GameObject gameobjectTest;
+    private List<GameObject> gameObjectList;
     public int _range = 2;
     // Start is called before the first frame update
     private void Awake()
@@ -23,11 +24,12 @@ public class RelayManager : MonoBehaviour
         }
         _relayBastTileData = new List<BaseTileData>();
         _constructiblesTiles = new List<BaseTileData>();
+        gameObjectList = new List<GameObject>();
 
     }
     public void RegisterStructure(BaseTileData structure)
     {
-        if(structure.structureTile != null && structure.structureTile.GetStructureType() == StructureType.Relay)
+        if (structure.structureTile != null && structure.structureTile.GetStructureType() == StructureType.Relay)
         {
             _relayBastTileData.Add(structure);
             ComputeConstructibleTerrainTiles();
@@ -35,33 +37,43 @@ public class RelayManager : MonoBehaviour
     }
     public void UnregisterStructure(BaseTileData structure)
     {
-         _relayBastTileData.Remove(structure);
-        ComputeConstructibleTerrainTiles();
-        //TODO deactivate structure outside of range ( comparer les deux list)
+        _relayBastTileData.Remove(structure);
+        IEnumerable<BaseTileData> intersection = ComputeConstructibleTerrainTiles();
+        foreach (BaseTileData baseTile in intersection)
+        {
+            if (baseTile.structureTile != null)
+            {
+                baseTile.structureTile.DeactivateStructureIfPossible();
+            }
+        }
     }
 
-    private void ComputeConstructibleTerrainTiles()
+    //returns the difference between old computation and new one
+    List<BaseTileData> ComputeConstructibleTerrainTiles()
     {
+        IEnumerable<BaseTileData> intersection = _constructiblesTiles;
         _constructiblesTiles.ToList().Clear();
+        gameObjectList.ForEach(x => Destroy(x));
         List<Vector3Int> uniqueList = new List<Vector3Int>();
-        foreach(BaseTileData structureTile in _relayBastTileData)
+        foreach (BaseTileData structureTile in _relayBastTileData)
         {
             List<Vector3Int> list = new List<Vector3Int>();
             list = GridUtils.GetNeighboursPositionsAtDistance(structureTile.gridPosition, _range);
-            foreach(Vector3Int vecInt in list)
+            foreach (Vector3Int vecInt in list)
             {
-                if(!uniqueList.Contains(vecInt))
+                if (!uniqueList.Contains(vecInt))
                 {
                     uniqueList.Add(vecInt);
                 }
             }
         }
         _constructiblesTiles = TilesDataManager.Instance.GetTilesAtPos(uniqueList);
-        foreach(BaseTileData baseTile in _constructiblesTiles)
+        foreach (BaseTileData baseTile in _constructiblesTiles)
         {
-            Instantiate(gameobjectTest, baseTile.worldPosition, Quaternion.identity);
+            gameObjectList.Add(Instantiate(gameobjectTest, baseTile.worldPosition, Quaternion.identity));
         }
-        
+        intersection = intersection.Except(_constructiblesTiles);
+        return intersection.ToList();
     }
 
     public bool IsInsideRelayRange(BaseTileData basetileData)
