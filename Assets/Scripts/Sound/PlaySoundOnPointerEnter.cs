20e1f3b1fe614868d801be52962da5bf84fd6fe0
Adrien Janvier <janvier.adrien@gmail.com>
2020-04-09 20:04:41 +0200

    FMOD correctly integrated + sounds on main menu buttons

---
+++ Assets/Scripts/Sound/PlaySoundOnPointerEnter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using FMODUnity;
+
+public class PlaySoundOnPointerEnter : MonoBehaviour, IPointerEnterHandler
+{
+    [EventRef]
+    public string eventName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("Pointer enter");
+        RuntimeManager.PlayOneShot(eventName);
+    }
+}
