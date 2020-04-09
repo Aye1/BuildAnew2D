0985a470974cf9d3d84f20822dc65a2a2abbcfa1
Adrien Janvier <janvier.adrien@gmail.com>
2020-04-09 20:44:17 +0200

    Can create Button template (with sound) from contextual menu

---
+++ Assets/Scripts/Editor/UITemplatesContextMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class UITemplatesContextMenu : MonoBehaviour
+{
+    [MenuItem("GameObject/BuildAnew/Button", false, 10)]
+    static void CreateButtonTemplate(MenuCommand menuCommand)
+    {
+        GameObject go = (GameObject) Resources.Load("TemplatesGameObjects/ButtonTemplate");
+        GameObject newGo = Instantiate(go);
+        newGo.name = "Button (BA)";
+        GameObjectUtility.SetParentAndAlign(newGo, menuCommand.context as GameObject);
+        Undo.RegisterCreatedObjectUndo(newGo, "Create " + newGo.name);
+        Selection.activeObject = newGo;
+    }
+}
--- Assets/Scripts/Sound/PlaySoundOnPointerEnter.cs
+++ Assets/Scripts/Sound/PlaySoundOnPointerEnter.cs
@@ -1,8 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using System;
-using UnityEngine;
-using UnityEngine.UI;
+﻿using UnityEngine;
 using UnityEngine.EventSystems;
 using FMODUnity;
 
@@ -11,21 +7,8 @@ public class PlaySoundOnPointerEnter : MonoBehaviour, IPointerEnterHandler
     [EventRef]
     public string eventName;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void OnPointerEnter(PointerEventData eventData)
     {
-        Debug.Log("Pointer enter");
         RuntimeManager.PlayOneShot(eventName);
     }
 }
