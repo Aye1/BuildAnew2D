85e4ff258c6963825143a92a75602b45b31f0e95
Adrien Janvier <janvier.adrien@gmail.com>
2020-04-11 15:19:33 +0200

    Try to add outline on UI

---
+++ Assets/Scripts/UI/OutlineWhenHover.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class OutlineWhenHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    private Material _outlineMaterial;
+
+    private void Start()
+    {
+        _outlineMaterial = GetComponent<Renderer>().material;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if(_outlineMaterial != null)
+        {
+            _outlineMaterial.SetFloat("_OutlineActive", 1.0f);
+        }
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if(_outlineMaterial != null)
+        {
+            _outlineMaterial.SetFloat("_OutlineActive", 0.0f);
+        }
+    }
+}
