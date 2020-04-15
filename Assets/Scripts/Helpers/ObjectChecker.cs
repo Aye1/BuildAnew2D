fc97120a9103baabb2daddd51cb720e1635b8b04
Adrien Janvier <janvier.adrien@gmail.com>
2020-04-15 14:25:14 +0200

    Add Dialogs (from NeoGaia)

---
+++ Assets/Scripts/Helpers/ObjectChecker.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public static class ObjectChecker
+{
+    public static void CheckNullity(Object obj, string error)
+    {
+        if (obj == null)
+        {
+            Debug.LogError(error);
+        }
+    }
+}
